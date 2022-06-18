587f879ec2feb0c2971b4b438b62b0c66f69e18f
quintin <2449164582@qq.com>
2022-06-18 22:31:07 +0800

    添加自动安装 packer 插件功能

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd 'packadd packer.nvim'
+end
+
 -- 基础配置
 require('core/basic')
 
@@ -13,6 +21,7 @@ require('configs/lsp')
 require('configs/dap')
 require('configs/nvim-treesitter')
 require('configs/symbols-outline')
+require('configs/rest')
 require('impatient')
 
 -- 主题配置
