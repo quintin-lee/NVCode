84a6f5c040656b3008bb060b9fc5fc92a4b279e9
quintin <2449164582@qq.com>
2022-06-22 18:38:33 +0800

    实现自动关闭 nvim-tree 插件

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -170,3 +170,13 @@ require 'nvim-tree'.setup {
         },
     },
 } -- END_DEFAULT_OPTS
+
+-- auto close
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+            vim.cmd "quit"
+        end
+    end
+})
