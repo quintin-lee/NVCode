bb38500f58133424071f5558f9a7225fe4680095
quintin <2449164582@qq.com>
2022-10-19 09:43:14 +0800

    更新 lsp 插件配置 api

--- lua/configs/lsp/init.lua
+++ lua/configs/lsp/init.lua
@@ -11,7 +11,7 @@ require("nvim-lsp-installer").setup({
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local lspconfig = require('lspconfig')
 
