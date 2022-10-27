e127a13b91f5fa2a6799bb288cbaf58d5fea0b46
quintin <2449164582@qq.com>
2022-10-27 10:31:25 +0800

    美化 which-key 插件

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -11,6 +11,22 @@ local goto_preview = require('goto-preview')
 
 local home = os.getenv('HOME')
 
+wk.setup({
+    window = {
+        border = "double", -- none, single, double, shadow
+        position = "bottom", -- bottom, top
+        margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
+        padding = { 3, 3, 3, 3 }, -- extra window padding [top, right, bottom, left]
+        winblend = 5,
+    },
+    layout = {
+        height = { min = 4, max = 25 }, -- min and max height of the columns
+        width = { min = 20, max = 50 }, -- min and max width of the columns
+        spacing = 3, -- spacing between columns
+        align = "left", -- align columns left, center or right
+    },
+})
+
 wk.register({
     w = {
         name = 'Workspace',
