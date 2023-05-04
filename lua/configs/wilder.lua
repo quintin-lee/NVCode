77aed132ef4239c93f9de8edf6426f6559adf9b8
quintin <2449164582@qq.com>
2023-05-04 16:22:04 +0800

    🔧 config(wilder): update cmd wildmenu to wildmenu_renderer

--- lua/configs/wilder.lua
+++ lua/configs/wilder.lua
@@ -13,28 +13,10 @@ wilder.set_option('pipeline', {
   )
 })
 
-local popupmenu_renderer = wilder.popupmenu_renderer(
-  wilder.popupmenu_border_theme({
-    border = 'rounded',
-    empty_message = wilder.popupmenu_empty_message_with_spinner(),
-    highlighter = wilder.lua_fzy_highlighter(),
-    left = {
-      ' ',
-      wilder.popupmenu_devicons(),
-      wilder.popupmenu_buffer_flags({
-        flags = ' a + ',
-        icons = {['+'] = '', a = '', h = ''},
-      }),
-    },
-    right = {
-      ' ',
-      wilder.popupmenu_scrollbar(),
-    },
-  })
-)
-
 wilder.set_option('renderer', wilder.renderer_mux({
-  [':'] = popupmenu_renderer,
+  [':'] = wilder.wildmenu_renderer({
+    highlighter = wilder.lua_fzy_highlighter(),
+  }),
   ['/'] = wilder.wildmenu_renderer({
     highlighter = wilder.lua_fzy_highlighter(),
   }),
