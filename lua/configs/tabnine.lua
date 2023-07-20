6acf5ab9e8a619083eb6025346b55345944ebc02
quintin <2449164582@qq.com>
2023-07-20 10:22:13 +0800

    🔧 config(tabnine): 修改 tabnine 快捷键

--- lua/configs/tabnine.lua
+++ lua/configs/tabnine.lua
@@ -1,6 +1,6 @@
 require('tabnine').setup({
   disable_auto_comment=true,
-  accept_keymap="<C-N>",
+  accept_keymap="<C-C>",
   dismiss_keymap = "<C-L]>",
   debounce_ms = 800,
   suggestion_color = {gui = "#808080", cterm = 244},
