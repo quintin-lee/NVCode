1b64b08ae72e436fe453670773e0a39bb010015b
quintin <quintin@lee.com>
2024-10-29 14:20:13 +0800

    🔧 config(which-key): Improving Layout Aesthetics

--- lua/configs/keymap.lua
+++ lua/configs/keymap.lua
@@ -11,19 +11,25 @@ local goto_preview = require('goto-preview')
 local config_home = vim.fn.stdpath("config")
 
 wk.setup({
-    window = {
-        border = "double", -- none, single, double, shadow
-        position = "bottom", -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-        padding = { 1, 2, 1, 2 }, -- extra window padding [top, right, bottom, left]
-        winblend = 0,  -- value between 0-100 0 for fully opaque and 100 for fully transparent
-        zindex = 1000, -- positive value to position WhichKey above other floating windows.
+    ---@type false | "classic" | "modern" | "helix"
+    preset = "modern",
+    ---@type wk.Win.opts
+    win = {
+      -- don't allow the popup to overlap with the cursor
+      no_overlap = true,
+      padding = { 1, 2 }, -- extra window padding [top/bottom, right/left]
+      title = true,
+      title_pos = "center",
+      zindex = 1000,
+      -- Additional vim.wo and vim.bo options
+      bo = {},
+      wo = {
+        -- winblend = 10, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+      },
     },
     layout = {
-        height = { min = 4, max = 25 }, -- min and max height of the columns
-        width = { min = 20, max = 50 }, -- min and max width of the columns
-        spacing = 3, -- spacing between columns
-        align = "left", -- align columns left, center or right
+        width = { min = 25, max = 50 }, -- min and max width of the columns
+        spacing = 5, -- spacing between columns
     },
 })
 
