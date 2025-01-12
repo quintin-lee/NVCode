9f42814eee52272ed3f6f3bf422391a056867bed
quintin <2449164582@qq.com>
2025-01-12 12:20:52 +0800

    🔧 config(avante): update avante plugin

--- lua/configs/ai/avante.lua
+++ lua/configs/ai/avante.lua
@@ -10,30 +10,11 @@ require('avante').setup({
       refresh = "nr", -- refresh
   },
   vendors = {
-    ---@type AvanteProvider
     ollama = {
-      ["local"] = true,
-      endpoint = endpoint,
-      model = model,
-      parse_curl_args = function(opts, code_opts)
-        return {
-          url = opts.endpoint .. "/chat/completions",
-          headers = {
-            ["Accept"] = "application/json",
-            ["Content-Type"] = "application/json",
-            ["Authorization"] = "Bearer " .. api_key,
-          },
-          body = {
-            model = opts.model,
-            messages = require("avante.providers").copilot.parse_message(code_opts), -- you can make your own message, but this is very advanced
-            max_tokens = 4096,
-            stream = true,
-          },
-        }
-      end,
-      parse_response_data = function(data_stream, event_state, opts)
-        require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-      end,
+        __inherited_from = "openai",
+        api_key_name = "AVANTE_API_KEY",
+        endpoint = endpoint,
+        model = model,
     },
   }
 })
