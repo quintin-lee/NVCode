17c2225684b3ddb2c874828019677b29a095a944
quintin <2449164582@qq.com>
2025-01-19 20:23:39 +0800

    ♻️ recycle(head): Refactor file header insertion and add C language file header

--- lua/header/header.lua
+++ lua/header/header.lua
@@ -1,18 +1,48 @@
 local config_home = vim.fn.stdpath("config")
 
-vim.api.nvim_create_autocmd('BufNewFile', {
-  pattern = '*.py',
-  command = '0r ' ..config_home.. '/lua/header/templates/python.txt'
-})
+-- 创建一个自动命令组来管理所有模板相关的自动命令
+local template_augroup = vim.api.nvim_create_augroup('TemplateAutoCommands', { clear = true })
 
-vim.api.nvim_create_autocmd('BufNewFile', {
-  pattern = '*.sh',
-  command = '0r ' ..config_home.. '/lua/header/templates/bash.txt'
-})
-
-vim.api.nvim_create_autocmd('BufNewFile', {
-  pattern = '*',
-  callback = function()
-    vim.api.nvim_command('normal G')
+-- 模板插入函数
+local function insert_template(pattern, template_path)
+  -- 检查模板文件是否存在
+  if vim.fn.filereadable(template_path) == 0 then
+    vim.notify(string.format("Template file not found: %s", template_path), vim.log.levels.WARN)
+    return
   end
-})
+
+  vim.api.nvim_create_autocmd('BufNewFile', {
+    pattern = pattern,
+    callback = function()
+      -- 插入模板文件内容
+      vim.cmd('0r ' .. template_path)
+
+      -- 查找 <!-- CURSOR --> 标记并移动光标到该位置
+      local cursor_pos = vim.fn.search('<\\!-- CURSOR -->', 'n')
+      if cursor_pos > 0 then
+        vim.api.nvim_win_set_cursor(0, {cursor_pos, 0})
+        -- 删除 <!-- CURSOR --> 标记
+        vim.cmd(cursor_pos .. 'delete _')
+      else
+        -- 如果没有找到 <!-- CURSOR --> 标记，跳转到文件末尾
+        local line_count = vim.api.nvim_buf_line_count(vim.api.nvim_get_current_buf())
+        if line_count > 0 then
+          vim.api.nvim_win_set_cursor(0, {line_count, 0})
+        end
+      end
+    end,
+    group = template_augroup
+  })
+end
+
+-- 注册模板
+local templates = {
+  ['*.py'] = '/lua/header/templates/python.txt',
+  ['*.sh'] = '/lua/header/templates/bash.txt',
+  ['*.c'] = '/lua/header/templates/c.txt'
+}
+
+-- 注册所有模板
+for pattern, template in pairs(templates) do
+  insert_template(pattern, config_home .. template)
+end
