e855529cdd4c2833d34482300825c9add7838fe8
quintin <2449164582@qq.com>
2025-01-24 20:27:00 +0800

    ✨ feat(cmp): update nvim-cmp plugin and optimize configuration

--- lua/configs/lsp/init.lua
+++ lua/configs/lsp/init.lua
@@ -80,6 +80,17 @@ local cmp_kinds = {
 }
 
 cmp.setup {
+    enabled = function()
+      -- disable completion in comments
+      local context = require 'cmp.config.context'
+      -- keep command mode completion enabled when cursor is in a comment
+      if vim.api.nvim_get_mode().mode == 'c' then
+        return true
+      else
+        return not context.in_treesitter_capture("comment") 
+          and not context.in_syntax_group("Comment")
+      end
+    end,
     completion = {
         autocomplete = false,
         --autocomplete = {
