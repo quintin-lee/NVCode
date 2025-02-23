5389492ca9064e7110f637f5a2fcbb6a8ca637f3
quintin <2449164582@qq.com>
2025-02-23 20:04:14 +0800

    ✨ feat(git): add Git Blame virtual text at the end of lines

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -16,11 +16,11 @@ return {
                 follow_files = true
             },
             attach_to_untracked = true,
-            current_line_blame = false,
+            current_line_blame = true,
             current_line_blame_opts = {
                 virt_text = true,
                 virt_text_pos = 'eol',
-                delay = 1000,
+                delay = 500,
                 ignore_whitespace = false,
             },
             sign_priority = 6,
