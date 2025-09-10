764f5e826c564803832ff15fa5be3429b13502d0
quintin <quintin@lee.com>
2025-09-10 17:01:52 +0800

    ♻️ recycle(avante): drop API key env, add optional deps.

--- lua/plugins/ai/avante.lua
+++ lua/plugins/ai/avante.lua
@@ -6,7 +6,6 @@ return {
     config = function()
         local endpoint = tostring(os.getenv("AVANTE_API_ENDPOINT"))
         local model    = tostring(os.getenv("AVANTE_MODEL_NAME"))
-        local api_key  = tostring(os.getenv("AVANTE_API_KEY"))
 
         require('avante').setup({
             provider = "openrouter",
@@ -43,4 +42,40 @@ return {
             end,
         })
     end,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        --- 以下依赖项是可选的，
+        "echasnovski/mini.pick", -- 用于文件选择器提供者 mini.pick
+        "nvim-telescope/telescope.nvim", -- 用于文件选择器提供者 telescope
+        "hrsh7th/nvim-cmp", -- avante 命令和提及的自动完成
+        "ibhagwan/fzf-lua", -- 用于文件选择器提供者 fzf
+        "nvim-tree/nvim-web-devicons", -- 或 echasnovski/mini.icons
+        "zbirenbaum/copilot.lua", -- 用于 providers='copilot'
+        {
+            -- 支持图像粘贴
+            "HakonHarnes/img-clip.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- 推荐设置
+                default = {
+                    embed_image_as_base64 = false,
+                    prompt_for_file_name = false,
+                    drag_and_drop = {
+                        insert_mode = true,
+                    },
+                    -- Windows 用户必需
+                    use_absolute_path = true,
+                },
+            },
+        },
+        {
+            -- 如果您有 lazy=true，请确保正确设置
+            'MeanderingProgrammer/render-markdown.nvim',
+            opts = {
+                file_types = { "markdown", "Avante" },
+            },
+            ft = { "markdown", "Avante" },
+        },
+    },
 }
