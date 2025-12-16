cc7791171f110dc19526a9fb08871dce1d4de391
quintin <2449164582@qq.com>
2025-12-16 21:24:03 +0800

    🔧 config(plugins): update plugins

--- lua/plugins/editor/leap.lua
+++ lua/plugins/editor/leap.lua
@@ -2,7 +2,12 @@ return {
     'ggandor/leap.nvim',
     event = "VeryLazy",
     config = function()
-        require('leap').add_default_mappings()
+        require('leap')
+        -- 单次跳转：s/S 向下/向上
+        vim.keymap.set({'n','x','o'}, 's',  '<Plug>(leap-forward)')
+        vim.keymap.set({'n','x','o'}, 'S',  '<Plug>(leap-backward)')
+        -- 跨窗口：gs
+        vim.keymap.set({'n','x','o'}, 'gs', '<Plug>(leap-cross-window)')
         -- 自定义颜色
         vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
         vim.api.nvim_set_hl(0, 'LeapMatch', {
