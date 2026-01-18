ba08a3299b5feaabcb495ba7e3323844293838f6
quintin <2449164582@qq.com>
2026-01-18 15:58:12 +0800

    feat(tools): 添加浮动终端快捷键

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,7 @@
 -- Add any additional keymaps here
 
 local map = vim.keymap.set
+
+-- FTerm 快捷键配置 (兼容 NVCode 习惯)
+map("n", "<A-i>", '<CMD>lua require("FTerm").toggle()<CR>', { desc = "Toggle Terminal" })
+map("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', { desc = "Toggle Terminal" })
--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -4,12 +4,4 @@ return {
     "numToStr/FTerm.nvim",
     opts = { border = "double", dimensions = { height = 0.9, width = 0.9 } },
   },
-  -- 翻译插件
-  {
-    "voldikss/vim-translator",
-    keys = {
-      { "<leader>ts", "<Plug>TranslateW", desc = "Translate Window" },
-      { "<leader>tr", "<Plug>TranslateR", desc = "Translate Replace" },
-    },
-  },
 }
