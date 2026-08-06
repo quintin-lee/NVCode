71ff90487eda2a2ce1f9a9bd09cf103bd7f8dd1b
quintin <2449164582@qq.com>
2026-08-06 14:36:59 +0800

    fix(nvim): 🐛 disable gitsigns current_line_blame to remove duplicate blame display

--- lua/plugins/tools/git.lua
+++ lua/plugins/tools/git.lua
@@ -7,7 +7,7 @@ return {
     event = "LazyFile",
     enabled = true,
     opts = {
-      current_line_blame = true,
+      current_line_blame = false,
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol",
